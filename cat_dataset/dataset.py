0aa47fbca640bf17b9cc3093c4b6ab4171c95408
なるみ <weaper@gmail.com>
2018-09-11 17:21:23 +0800

    Align cat face

--- cat_dataset/dataset.py
+++ cat_dataset/dataset.py
@@ -1,5 +1,6 @@
 import glob
 import os
+from math import atan2, degrees, sqrt
 
 import numpy as np
 from PIL import Image
@@ -13,16 +14,58 @@ def load_image(f, mode='RGB'):
 
 def load_label(f):
     with open(f, 'r') as fp:
-        s = fp.read().strip()
-        return np.fromstring(s, dtype=int, sep=' ')[1:]
+        points = list(map(int, fp.read().strip().split(' ')))
+        return dict(
+            left_eye=(points[1], points[2]),
+            right_eye=(points[3], points[4]),
+            mouth=(points[5], points[6]),
+            left_ear_1=(points[7], points[8]),
+            left_ear_2=(points[9], points[10]),
+            left_ear_3=(points[11], points[12]),
+            right_ear_1=(points[13], points[14]),
+            right_ear_2=(points[15], points[16]),
+            right_ear_3=(points[17], points[18]))
+
+
+def distance(x, y):
+    return sqrt((x[0] - y[0])**2 + (x[1] - y[1])**2)
+
+
+def center(x, y):
+    return (x[0] + y[0]) / 2, (x[1] + y[1]) / 2
+
+
+def align(image, label):
+
+    left_eye = label['left_eye']
+    right_eye = label['right_eye']
+    mouth = label['mouth']
+
+    # compute degrees and rotate
+    x = right_eye[0] - left_eye[0]
+    y = right_eye[1] - left_eye[1]
+
+    theta = degrees(atan2(y, x))
+    image = image.rotate(theta, center=mouth)
+
+    # crop
+    w = distance(left_eye, right_eye)
+    h = distance(mouth, center(left_eye, right_eye))
+
+    image = image.crop(
+        box=(mouth[0] - int(w), mouth[1] - int(h * 2), mouth[0] + int(w),
+             mouth[1] + int(h * 0.7)))
+
+    return image
 
 
 class CatDataset(data.Dataset):
 
-    def __init__(self, root, transform=None):
+    def __init__(self, root, transform=None, align=True):
         self.root = root
         self.samples = None
         self.transform = transform
+        self.align = align
 
         self._prepare_samples()
 
@@ -32,18 +75,20 @@ class CatDataset(data.Dataset):
         for label_path in label_paths:
             image_path = label_path.rstrip('.cat')
             if os.path.exists(image_path):
-                label = load_label(label_path)
-                sample = image_path, label
-                self.samples.append(sample)
+                self.samples.append((image_path, label_path))
 
-    def __getitem__(self, index):
-        image_path, label = self.samples[index]
+    def __getitem__(self, index: int):
+        image_path, label_path = self.samples[index]
         image = load_image(image_path)
+        label = load_label(label_path)
+
+        if self.align:
+            image = align(image, label)
 
         if self.transform is not None:
             image = self.transform(image)
 
-        return image, label
+        return image
 
     def __len__(self):
         return len(self.samples)
